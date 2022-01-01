f04cb30270c2f0676e3e8dce06fe1e44f42f0294
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-01 20:14:32 +0100

    Add Wallet Connection Action

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import appLogo from './assets/gif-logo.png';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
@@ -8,6 +8,8 @@ const TWITTER_HANDLE = 'thefemiayodeji';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try{
       const {solana} = window;
@@ -16,6 +18,7 @@ const App = () => {
           console.log("Phantom wallet found!");
           const response = await solana.connect({ onlyIfTrusted: true});
           console.log("Connected with Public Key:", response.publicKey.toString());
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert("Solana object not found! Get a Phantom Wallet 👻")
@@ -25,7 +28,14 @@ const App = () => {
     }
   }
 
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const {solana} = window;
+    if(solana){
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
   const renderNotConnectedContainer = () => (
     <button 
@@ -46,7 +56,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">
             <img alt="GIF" className="app-logo" src={appLogo} />
@@ -55,7 +65,7 @@ const App = () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {renderNotConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <span className="footer-text">Built by </span>
