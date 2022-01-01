78fd6b46bd37bcbd49bd6a7bfb6c46c18fd6f7b3
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-01 19:38:08 +0100

    Add Phantom Wallent Check

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -6,6 +7,29 @@ const TWITTER_HANDLE = 'thefemiayodeji';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const {solana} = window;
+      if(solana){
+        if(solana.isPhantom){
+          console.log("Phantom wallet found!");
+        }
+      } else {
+        alert("SOlana object not found! Get a Phantom Wallet 👻")
+      }
+    } catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const onLoad =  async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad)
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
