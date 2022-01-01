2fca670b3967fa867a3cc0206b06da3f3d5a6b3a
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-01 21:58:30 +0100

    Add RenderConnectedContainer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ const TWITTER_HANDLE = 'thefemiayodeji';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  
+  const TEST_GIFS = [
+    'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+    'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+    'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+    'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+  ]
+
   const [walletAddress, setWalletAddress] = useState(null);
 
   const checkIfWalletIsConnected = async () => {
@@ -44,7 +52,21 @@ const App = () => {
     >
       Connect to Wallet
     </button>
-  )
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <div className="gif-grid">
+        {
+          TEST_GIFS.map(gif => (
+            <div className="gif-item" key={gif}>
+              <img src={gif} alt={gif} />
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 
   useEffect(() => {
     const onLoad =  async () => {
@@ -65,7 +87,10 @@ const App = () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+        </div>
+        <div className="gif-container">
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <span className="footer-text">Built by </span>
