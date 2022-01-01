036e05f32c17ad2b842dfa41296bec830089429a
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-01 18:49:04 +0100

    Modify Style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'thefemiayodeji';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -10,19 +10,23 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
         </div>
         <div className="footer-container">
+          <span className="footer-text">Built by </span>
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >
+            {`@${TWITTER_HANDLE}`}
+          </a>
+          <span  className="footer-text">All Rights Licensed</span>
         </div>
       </div>
     </div>
