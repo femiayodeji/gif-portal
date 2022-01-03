ed70ec458902a3b653adccd680d27b1f9e430d07
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-03 01:03:49 +0100

    Add GIF Retrieval From Solana on Devnet

--- src/App.js
+++ src/App.js
@@ -64,6 +64,20 @@ const App = () => {
     }
   };
 
+  const getGifList = async() => {
+    try{
+      const provider = getProvider();
+      const program =  new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+      console.log("Got the account", account)
+      setGifList(account.gifList)
+    } catch(error){
+      console.log("Error in getGifList: ", error);
+      setGifList(null);
+    }
+  }
+
   const sendGif = async () => {
     if(inputValue.length > 0){
       console.log("GIF link:", inputValue);
@@ -86,6 +100,7 @@ const App = () => {
      );
      return provider;
    }
+
   const renderNotConnectedContainer = () => (
     <button 
       className="cta-button connect-wallet-button"
@@ -132,10 +147,7 @@ const App = () => {
   useEffect(() => {
     if(walletAddress){
       console.log("Fetching GIF list...");
-
-      //Call Solana program here
-
-      setGifList(TEST_GIFS);
+      getGifList();
     }
   }, [walletAddress]);
 
